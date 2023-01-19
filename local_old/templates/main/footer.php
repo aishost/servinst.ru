b95fa8850dc8bd99317b5a30a3159a21980c0e5e
Ivan <aishost@ya.ru>
2023-01-19 22:56:44 +0300

    servinst

--- local_old/templates/main/footer.php
+++
@@ -1,1 +0,0 @@
-<?php if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();?> <script type="text/babel" src="<? echo SITE_TEMPLATE_PATH ?>/js/app.js"></script><!-- <script type="text/babel" src="<?php echo SITE_TEMPLATE_PATH ?>/js/react.js"></script> -->
